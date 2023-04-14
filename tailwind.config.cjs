5895497d4dae8a132d4f2820a2517c8a8082ceea
Gilbert Acosta <gisawebdev@gmail.com>
2023-04-14 14:48:10 -0400

    feat: add darkmode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,12 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {
-			sans: {
-				fontFamily: ['Noto Sans Mono', ...defaultTheme.fontFamily.sans],
-			},
+		extend: {},
+
+		fontFamily: {
+			mono: ["Noto Sans Mono", "system-ui", "monospace"],
 		},
 	},
 	plugins: [],
